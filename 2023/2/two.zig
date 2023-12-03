2855e1bb2029bb583fe21c953fdda1cc5da25f9e
Tomás Senart <tsenart@gmail.com>
2023-12-03 22:22:34 +0100

    2023: unvectorize 2 2

--- 2023/2/two.zig
+++ 2023/2/two.zig
@@ -4,19 +4,21 @@ const one = @import("one.zig");
 const debug = std.debug;
 
 pub fn main() !void {
+    const start = try std.time.Instant.now();
+
     var stdin = std.io.bufferedReader(std.io.getStdIn().reader());
     var sc = aoc.fixedBufferScanner(stdin.reader(), '\n', 4096);
     var sum: u64 = 0;
     const rounds = one.maxRounds;
     while (try sc.next()) |line| {
         const game = one.parseGame(line) orelse return;
-        const r: @Vector(rounds, u8) = game.colors[0..rounds].*;
-        const g: @Vector(rounds, u8) = game.colors[rounds .. 2 * rounds].*;
-        const b: @Vector(rounds, u8) = game.colors[2 * rounds ..].*;
-        sum +=
-            @as(u64, @reduce(.Max, r)) *
-            @as(u64, @reduce(.Max, g)) *
-            @as(u64, @reduce(.Max, b));
+        var power: u64 = 1;
+        for (0..3) |i| {
+            power *= std.mem.max(u8, game.colors[i * rounds .. (i + 1) * rounds]);
+        }
+        sum += power;
     }
-    debug.print("{d}\n", .{sum});
+
+    const took = std.time.Instant.since(try std.time.Instant.now(), start);
+    debug.print("Answer {d}, took {s}\n", .{ sum, std.fmt.fmtDuration(took) });
 }
