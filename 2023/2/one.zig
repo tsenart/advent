e77f32282b5da3338a0ddc45324e533f5a1611f3
Tomás Senart <tsenart@gmail.com>
2023-12-03 01:44:38 +0100

    2023: 2 one

---
+++ 2023/2/one.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+const ascii = std.ascii;
+const debug = std.debug;
+const mem = std.mem;
+const fmt = std.fmt;
+
+pub fn main() !void {
+    var buf: [4096]u8 = undefined;
+    var fbs = std.io.fixedBufferStream(&buf);
+    const writer = fbs.writer();
+    const stdin = std.io.getStdIn().reader();
+
+    const limits: [3 * maxRounds]u8 =
+        ([_]u8{12} ** maxRounds) ++ // Red
+        ([_]u8{13} ** maxRounds) ++ // Green
+        ([_]u8{14} ** maxRounds); // Blue
+
+    var sum: u64 = 0;
+    while (true) {
+        stdin.streamUntilDelimiter(writer, '\n', buf.len) catch |err| switch (err) {
+            error.EndOfStream => if (fbs.getWritten().len == 0) break,
+            else => {
+                debug.print("Error: {s}\n", .{@errorName(err)});
+                return;
+            },
+        };
+
+        const line = fbs.getWritten();
+        const game = parseGame(line) orelse return;
+        if (@reduce(.And, game.colors <= limits))
+            sum += game.id;
+
+        fbs.reset();
+    }
+
+    debug.print("{d}\n", .{sum});
+}
+
+const maxRounds = 7;
+const colors = [_][]const u8{ "red", "green", "blue" };
+const Game = struct { id: u8, colors: @Vector(maxRounds * 3, u8) };
+
+fn parseGame(line: []const u8) ?Game {
+    const hi = mem.indexOf(u8, line, ": ") orelse return null;
+    const lo = mem.lastIndexOfScalar(u8, line[0..hi], ' ') orelse return null;
+    const id = fmt.parseUnsigned(u8, line[lo + 1 .. hi], 10) catch return null;
+
+    var game = Game{ .id = id, .colors = [_]u8{0} ** (maxRounds * 3) };
+    var rounds = mem.splitSequence(u8, line[hi + 2 ..], "; ");
+    var i: usize = 0;
+
+    while (rounds.next()) |round| {
+        var draws = mem.splitSequence(u8, round, ", ");
+        while (draws.next()) |draw| {
+            const space = mem.indexOfScalar(u8, draw, ' ') orelse return null;
+            const num = fmt.parseUnsigned(u8, draw[0..space], 10) catch return null;
+            const color = for (colors, 0..) |color, j| {
+                if (mem.eql(u8, draw[space + 1 ..], color)) break j;
+            } else return null;
+            game.colors[color * maxRounds + i] = num;
+        }
+
+        i += 1;
+        if (i == maxRounds) {
+            debug.print("too many rounds: {s}\n", .{line});
+            return null;
+        }
+    }
+
+    return game;
+}
